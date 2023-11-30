f5fee99ef0101afab1f339d6c96c12db808d512d
Snap <sailersnap@mail.ru>
2023-11-30 15:25:06 +0600

    add users

---
+++ users/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from users.models import EmailVerification, User
+
+
+
+
+@admin.register(EmailVerification)
+class EmailVerificationAdmin(admin.ModelAdmin):
+    list_display = ('code', 'user', 'expiration')
+    fields = ('code', 'user', 'expiration', 'created')
+    readonly_fields = ('created',)
